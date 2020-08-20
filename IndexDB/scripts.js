efae84275257725fc74b923ba06a1c5dc4cc32de
anbreaker <antunez19@gmail.com>
2020-08-20 20:21:35 +0200

    indexDB learning basics

--- IndexDB/scripts.js
+++ IndexDB/scripts.js
@@ -1,6 +1,7 @@
 // // const iDB = window.indexedDB;
 const indexedDB = window.indexedDB;
 const form = document.getElementById('form');
+const tasks = document.getElementById('tasks');
 
 if (indexedDB) {
   let db;
@@ -9,13 +10,14 @@ if (indexedDB) {
   request.onsuccess = () => {
     db = request.result;
     console.log('OPEN', db);
+    readData();
   };
 
   request.onupgradeneeded = () => {
     db = request.result;
     console.log('Create IndexDataBase', db);
 
-    const objectStore = db.createObjectStore('tasks', {keyPath: 'taskTitle'});
+    const objectStore = db.createObjectStore('tasks', {autoIncrement: true});
   };
 
   request.onerror = (error) => {
@@ -23,9 +25,39 @@ if (indexedDB) {
   };
 
   const addData = (data) => {
-    const transaction = db.transaction('tasks', 'readwrite');
+    const transaction = db.transaction(['tasks'], 'readwrite');
     const objectStore = transaction.objectStore('tasks');
     const request = objectStore.add(data);
+    readData();
+  };
+
+  const readData = () => {
+    const transaction = db.transaction(['tasks'], 'readonly');
+    const objectStore = transaction.objectStore('tasks');
+    const request = objectStore.openCursor();
+
+    const fragment = document.createDocumentFragment();
+
+    request.onsuccess = (event) => {
+      const cursor = event.target.result;
+      if (cursor) {
+        // console.log(cursor.value);
+        const taskTitle = document.createElement('p');
+        taskTitle.textContent = cursor.value.taskTitle;
+        fragment.appendChild(taskTitle);
+
+        const taskPriority = document.createElement('p');
+        taskPriority.textContent = cursor.value.taskPriority;
+        fragment.appendChild(taskPriority);
+
+        cursor.continue();
+      } else {
+        console.log('No more data');
+        console.dir(fragment);
+        tasks.textContent = '';
+        tasks.appendChild(fragment);
+      }
+    };
   };
 
   form.addEventListener('submit', (event) => {
