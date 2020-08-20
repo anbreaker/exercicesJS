bc4dce58b552f7627f71532594a86ae76b9f0398
anbreaker <antunez19@gmail.com>
2020-08-20 09:40:47 +0200

    finishedTasks to pendingTask conclude

--- toDoListDragAndDrop/toDoListDragAndDrop.js
+++ toDoListDragAndDrop/toDoListDragAndDrop.js
@@ -9,6 +9,7 @@ const finishedTasks = document.getElementById('finished-tasks');
 //setData: Establece la informacion que queremos compartir
 //getData: Establece la informacion que queremos obtener
 
+// pendingTask to finishedTasks
 pendingTask.addEventListener('dragstart', (event) => {
   event.dataTransfer.setData('text/plain', event.target.id);
   // console.log(event.dataTransfer.getData('text'));
@@ -33,4 +34,30 @@ finishedTasks.addEventListener('drop', (event) => {
   const element = document.getElementById(event.dataTransfer.getData('text'));
   element.classList.remove('active');
   finishedTasks.appendChild(pendingTask.removeChild(element));
+});
+
+// finishedTasks to pendingTask
+finishedTasks.addEventListener('dragstart', (event) => {
+  event.dataTransfer.setData('text/plain', event.target.id);
+});
+
+finishedTasks.addEventListener('drag', (event) => {
+  event.target.classList.add('active');
+});
+
+finishedTasks.addEventListener('dragend', (event) => {
+  event.target.classList.remove('active');
+});
+
+// Los dos siguientes van juntos y hay que prevenir el comportamiento
+// por defecto del navegador dragover y drop inseparables!!
+pendingTask.addEventListener('dragover', (event) => {
+  event.preventDefault();
+});
+
+pendingTask.addEventListener('drop', (event) => {
+  event.preventDefault();
+  const elementFinsed = document.getElementById(event.dataTransfer.getData('text'));
+  elementFinsed.classList.remove('active');
+  pendingTask.appendChild(finishedTasks.removeChild(elementFinsed));
 });
