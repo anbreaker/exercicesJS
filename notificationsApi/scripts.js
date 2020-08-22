be6159abf848892b65b5fb0570e24629eaa93a68
anbreaker <antunez19@gmail.com>
2020-08-22 11:10:48 +0200

    utilizando notifications

--- notificationsApi/scripts.js
+++ notificationsApi/scripts.js
@@ -4,10 +4,34 @@
   https://www.flaticon.es/icono-gratis/en-todo-el-mundo_814513?term=world&page=1&position=18
 */
 
-console.log('hola q tal');
-
 const permissions = document.getElementById('permissions');
 
 permissions.addEventListener('click', () => {
-  Notification.requestPermission();
+  if (Notification.permission !== 'granted') {
+    getPermissions();
+  } else {
+    notifyPermissions();
+  }
 });
+
+const getPermissions = () => {
+  Notification.requestPermission()
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
+};
+
+const notifyPermissions = () => {
+  const options = {
+    body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+    icon: 'world.png',
+    data: 'Extra data',
+    tag: 'notification demo',
+  };
+  const notification = new Notification('Hello World', options);
+
+  notification.addEventListener('close', () => console.log('Close'));
+  notification.addEventListener('show', () => console.log('show'));
+
+  console.log(notification.data);
+  console.log(notification);
+};
