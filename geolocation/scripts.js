7f5507662a80fd2b149aa509c7d60d6f327480b0
anbreaker <antunez19@gmail.com>
2020-08-22 20:36:54 +0200

    mejorando geolocation...

--- geolocation/scripts.js
+++ geolocation/scripts.js
@@ -25,3 +25,18 @@ const getPosition = (position) => {
   geolocationDiv.appendChild(geo);
 };
 const error = (error) => console.log(error);
+
+const detectOS = () => {
+  const platform = navigator.platform.toLowerCase(),
+    iosPlatforms = ['iphone', 'ipad', 'ipod', 'ipod touch'];
+
+  if (platform.includes('mac')) return 'MacOS';
+  if (iosPlatforms.includes(platform)) return 'iOS';
+  if (platform.includes('win')) return 'Windows';
+  if (/android/.test(navigator.userAgent.toLowerCase())) return 'Android';
+  if (/linux/.test(platform)) return 'Linux';
+
+  return 'unknown';
+};
+
+console.log(detectOS());
